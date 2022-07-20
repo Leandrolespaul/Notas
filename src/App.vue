1595aaecb5ac5c3cc4f4ffeb14071bbab05f4027
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-07-19 22:19:04 -0300

    App Notas

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,58 @@
 import Button from "primevue/button/sfc";
 import Card from "primevue/card/sfc";
 import { reactive, ref } from "vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Markdown from "vue3-markdown-it";
 
+const source = "#Hello World";
 const notaInput = reactive({ descricao: "", titulo: "" });
-const notas = ref([]);
+const notas = ref(JSON.parse(localStorage.getItem("Notas"))); //JSON converte o array
 const editandoNota = ref(false);
 
-function adicionarNota() {
-  notas.value.push({ ...notaInput });
+function zeroAEsquerda(num) {
+  // função para ter o 0 a esquerda!
+  return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());
+  const min = zeroAEsquerda(data.getMinutes());
+  const seg = zeroAEsquerda(data.getSeconds());
+
+  return `Data: ${dia}/${mes}/${ano} Horário:  ${hora}:${min}:${seg}`;
+}
+
+function limparInput() {
   notaInput.descricao = "";
   notaInput.titulo = "";
 }
+
+function adicionarNota() {
+  const novaNota = {
+    titulo: notaInput.titulo,
+    descricao: notaInput.descricao,
+    dataCriacao: formataData(new Date()),
+  };
+  notas.value.push(novaNota);
+  limparInput();
+  localStorage.setItem("Notas", JSON.stringify(notas.value));
+}
+
 function removerNota(nota) {
   notas.value = notas.value.filter((obj) => obj !== nota);
+  localStorage.setItem("Notas", JSON.stringify(notas.value));
 }
+
 let notaObj = "";
+
 function salvarEdicao() {
   notaObj.titulo = notaInput.titulo;
   notaObj.descricao = notaInput.descricao;
-
-  // limpei o input aqui
-  notaInput.descricao = "";
-  notaInput.titulo = "";
+  limparInput();
+  localStorage.setItem("Notas", JSON.stringify(notas.value));
 
   editandoNota.value = false;
 }
@@ -43,7 +74,13 @@ function editar(nota) {
     <input v-model="notaInput.titulo" placeholder="Título" />
   </div>
   <div style="margin: 10px">
-    <input v-model="notaInput.descricao" placeholder="Descrição" />
+    <textarea
+      style="resize: none"
+      rows="4"
+      cols="23"
+      v-model="notaInput.descricao"
+      placeholder="Descrição"
+    />
   </div>
 
   <Button
@@ -59,11 +96,12 @@ function editar(nota) {
     @click="adicionarNota"
   />
   <Card v-for="nota in notas" style="width: 25rem; margin-bottom: 2em">
-    <template #title> {{ nota.titulo }} </template>
+    <template #title> {{ nota.titulo }}</template>
     <template #content>
-      <p>
-        {{ nota.descricao }}
-      </p>
+      <h3>
+        <p><Markdown :source="nota.descricao" /></p>
+      </h3>
+      <div>{{ nota.dataCriacao }}</div>
     </template>
     <template #footer>
       <Button
