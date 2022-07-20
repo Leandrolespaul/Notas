4d1d215e872d9a4e3e869ca5a02fe2b4bf38fc81
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-07-19 22:18:46 -0300

    App Notas

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import Markdown from 'vue3-markdown-it';
+
 
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.mount('#app');
+app.use(Markdown);
